4eb9e8a3d876f133a1c10075f223a6bc9b30b108
Dane Worsham <zenki14@gmail.com>
2015-07-01 11:26:24 -0500

    working on mobile platform

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,12 +4,20 @@
   //background color
   $(window).scroll(function () {
     var scroll = $(document).scrollTop();
-    if ( scroll > 517 ) {
+    if ( scroll > 516 ) {
       $('header').css({"background": "#394053"});
     } else {
       $('header').css({"background": "none"});
     }
+    if ( scroll > 100 ) {
+      $('h1').css({"opacity": 0});
+    } else { 
+      $('h1').css({"opacity": 1});
+
+    }
   });
+
+
 
   // menu button
   var menuButton = document.getElementById('menuButton');
@@ -21,6 +29,14 @@
       } else {
         $('nav').hide();
       }
+  });
+
+  $(".goto").click(function(e) {
+      var section = e.currentTarget.attributes[0].value
+      var headerHeight = $('header').outerHeight();
+      $('html, body').animate({
+          scrollTop: $(section).offset().top - headerHeight
+      }, 400);
   });
 
 }());
